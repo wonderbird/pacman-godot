76f1277f2e68d87e417f191171e3953362275cd5
Monstroxx <152517394+Monstroxx@users.noreply.github.com>
2024-05-01 14:46:13 +0200

    kein diagonal mehr
    Co-Authored-By: Stefan Boos <kontakt@boos.systems>

--- scripts/player.gd
+++ scripts/player.gd
@@ -11,11 +11,11 @@ func _process(delta):
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
-	if Input.is_action_pressed("move_down"):
+	elif Input.is_action_pressed("move_down"):
 		velocity.y += 1
-	if Input.is_action_pressed("move_right"):
+	elif Input.is_action_pressed("move_right"):
 		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
+	elif Input.is_action_pressed("move_left"):
 		velocity.x -= 1
 	
 	if velocity.length() > 0:
